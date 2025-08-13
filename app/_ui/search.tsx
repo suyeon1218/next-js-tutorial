2ea3e338a5208a989620d476d14124529b118444
Suyeon <msjgr2801@gmail.com>
2025-08-13 12:44:04 -0700

    feat: 서치바로 검색 시 페이지네이션 1로 초기화하기

--- app/_ui/search.tsx
+++ app/_ui/search.tsx
@@ -10,6 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
