1fba36b309e0be339fd1cefa64f9e5597025fe75
Suyeon <msjgr2801@gmail.com>
2025-08-13 12:14:38 -0700

    feat: Invoices 페이지 초기 UI 구현하기

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import { lusitana } from '@/app/_ui/_fonts';
+import Search from '@/app/_ui/search';
+import { CreateInvoice } from './buttons';
+import Pagination from './pagination';
+import Table from './table';
+
+export default async function Page() {
+  return (
+    <div className='w-full'>
+      <div className='flex w-full items-center justify-between'>
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className='mt-4 flex items-center justify-between gap-2 md:mt-8'>
+        <Search placeholder='Search invoices...' />
+        <CreateInvoice />
+      </div>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className='mt-5 flex w-full justify-center'>
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
