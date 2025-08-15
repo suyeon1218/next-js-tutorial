ccae66a7e97db83573304e85a784f169472201d5
Suyeon <msjgr2801@gmail.com>
2025-08-15 14:49:39 -0700

    feat: 유저 유효성 검사 server-action 함수 추가

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { createClient } from '@supabase/supabase-js';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const supabase = createClient(
   process.env.SUPABASE_URL!,
@@ -122,4 +124,23 @@ export async function deleteInvoice(id: string) {
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
