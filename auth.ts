2927e3328775f3f5fc3e2fd0c83a2ca30a501477
Suyeon <msjgr2801@gmail.com>
2025-08-15 14:48:12 -0700

    feat: bycripto를 사용하여 유저 password 비교하기

---
+++ auth.ts
@@ -0,0 +1,49 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+import type { User } from './app/_lib/definitions';
+import bcrypt from 'bcrypt';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
+
+async function getUserFromSupabase(email: string): Promise<User | null> {
+  const {
+    data: { user },
+    error,
+  } = await supabase.from('users').select('*').eq('email', email).single();
+  if (error) {
+    console.error(error);
+    throw new Error('User not found');
+  }
+  return user;
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUserFromSupabase(email);
+          if (!user) {
+            return null;
+          }
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+});
